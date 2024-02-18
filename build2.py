35ab72b28b4268665787a27e2524ab65294dddb8
Tom Kerr <tomkerrdev@gmail.com>
2024-02-18 10:41:55 +0000

    fix: build

---
+++ build2.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+from distutils.command.build_ext import build_ext
+from distutils.core import Distribution, Extension
+
+import numpy
+from Cython.Build import cythonize
+
+link_args = []
+include_dirs = [numpy.get_include()]
+libraries = []
+
+
+def build():
+    source_files = []
+    for root, directories, files in os.walk("cython_extensions"):
+        for file in files:
+            if file.endswith("pyx"):
+                source_files.append(os.path.join(root, file))
+
+    extensions = cythonize(
+        Extension(
+            name="cython_extensions.bootstrap",
+            sources=source_files,
+            include_dirs=include_dirs,
+        ),
+        compiler_directives={"binding": True, "language_level": 3},
+    )
+
+    distribution = Distribution({"name": "extended", "ext_modules": extensions})
+    distribution.package_dir = "extended"
+
+    cmd = build_ext(distribution)
+    cmd.ensure_finalized()
+    cmd.run()
+
+    # Copy built extensions back to the project
+    for output in cmd.get_outputs():
+        relative_extension = os.path.relpath(output, cmd.build_lib)
+        shutil.copyfile(output, relative_extension)
+        mode = os.stat(relative_extension).st_mode
+        mode |= (mode & 0o444) >> 2
+        os.chmod(relative_extension, mode)
+
+
+if __name__ == "__main__":
+    build()
